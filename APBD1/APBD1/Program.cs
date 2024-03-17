c6842aeffda0c21d2314f9af8a8f2c98305f15d3
Oannamari <131410708+Oannamari@users.noreply.github.com>
2024-03-17 20:07:29 +0100

    Add GetMax method

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -15,6 +15,20 @@ static double GetAverage(int[] numbers) {
     return (double)sum / numbers.Length;
 }
 
+static int GetMax(int[] numbers) {
+    int max = 0;
+    foreach (var number in numbers) {
+        if (number > max) {
+            max = number;
+        }
+    }
+
+    return max;
+}
+
 int[] numbers = {1, 2, 3, 4, 5};
 double average = GetAverage(numbers);
 Console.WriteLine(average);
+
+int max = GetMax(numbers);
+Console.WriteLine(max);
